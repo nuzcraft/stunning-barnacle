f0a244287acbb22816e572639d533d821370107b
nuzcraft <nuzcraft@gmail.com>
2024-03-02 21:31:46 -0600

    added breakable walls

--- scenes/world.gd
+++ scenes/world.gd
@@ -34,20 +34,19 @@ func _process(delta: float) -> void:
 				border_gradient_alpha(1.0)
 		ACTION:
 			if Input.is_action_just_pressed("up"):
-				var destination = hero.position + (Vector2(0,-1) * tilesize)
-				tile_map.set_cell_broken_wall(tile_map.local_to_map(destination))
+				try_attack_wall(hero, Vector2(0, -1))
 				gamestate = MOVING
 				border_gradient_alpha(0.0)
 			if Input.is_action_just_pressed("down"):
-				print("down")
+				try_attack_wall(hero, Vector2(0, 1))
 				gamestate = MOVING
 				border_gradient_alpha(0.0)
 			if Input.is_action_just_pressed("left"):
-				print("left")
+				try_attack_wall(hero, Vector2(-1, 0))
 				gamestate = MOVING
 				border_gradient_alpha(0.0)
 			if Input.is_action_just_pressed("right"):
-				print("right")
+				try_attack_wall(hero, Vector2(1, 0))
 				gamestate = MOVING
 				border_gradient_alpha(0.0)
 		
@@ -67,4 +66,12 @@ func try_move(actor: Actor, vector: Vector2) -> bool:
 func border_gradient_alpha(alpha: float) -> void:
 	var tween = get_tree().create_tween()
 	tween.tween_property(border_gradient, "modulate:a", alpha, 0.25)
+	
+func try_attack_wall(actor: Actor, vector: Vector2) -> bool:
+	var destination = actor.position + (vector * tilesize)
+	var coords = tile_map.local_to_map(destination)
+	var success = tile_map.try_attack_wall(coords)
+	if success:
+		actor.bump_anim(vector)
+	return success
 		
