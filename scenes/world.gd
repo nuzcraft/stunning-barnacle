75dda190dbad4f21c774bdab5878503cc86e1f71
nuzcraft <nuzcraft@gmail.com>
2024-03-05 07:31:30 -0600

    added enemy turn

--- scenes/world.gd
+++ scenes/world.gd
@@ -13,44 +13,32 @@ enum {
 	SUCK
 }
 var gamestate = MOVING
+var turn_taken = false
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	prep_astar_grid()
+	for enemy in get_tree().get_nodes_in_group("enemies"):
+		enemy.target = tile_map.local_to_map(hero.position)
 	pass # Replace with function body.
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	match gamestate:
-		MOVING:
-			if Input.is_action_just_pressed("up"):
-				try_move(hero, Vector2(0, -1))
-			if Input.is_action_just_pressed("down"):
-				try_move(hero, Vector2(0, 1))
-			if Input.is_action_just_pressed("left"):
-				try_move(hero, Vector2(-1, 0))
-			if Input.is_action_just_pressed("right"):
-				try_move(hero, Vector2(1, 0))
-			if Input.is_action_just_pressed("action"):
-				gamestate = ACTION
-				border_gradient_alpha(1.0)
-		ACTION:
-			if Input.is_action_just_pressed("up"):
-				try_attack_wall(hero, Vector2(0, -1))
-				gamestate = MOVING
-				border_gradient_alpha(0.0)
-			if Input.is_action_just_pressed("down"):
-				try_attack_wall(hero, Vector2(0, 1))
-				gamestate = MOVING
-				border_gradient_alpha(0.0)
-			if Input.is_action_just_pressed("left"):
-				try_attack_wall(hero, Vector2(-1, 0))
-				gamestate = MOVING
-				border_gradient_alpha(0.0)
-			if Input.is_action_just_pressed("right"):
-				try_attack_wall(hero, Vector2(1, 0))
-				gamestate = MOVING
-				border_gradient_alpha(0.0)
+	if not turn_taken:
+		match gamestate:
+			MOVING:
+				hero_moving()
+			ACTION:
+				hero_action()
+	if turn_taken:
+		for enemy in get_tree().get_nodes_in_group("enemies"):
+			var path: Array[Vector2i] = astar_grid.get_id_path(tile_map.local_to_map(enemy.position), enemy.target)
+			if path.size() > 1:
+				var vector = path[1] - path[0]
+				try_move(enemy, vector)
+		for enemy in get_tree().get_nodes_in_group("enemies"):
+			enemy.target = tile_map.local_to_map(hero.position)
+		turn_taken = false
 		
 func try_move(actor: Actor, vector: Vector2) -> bool:
 	var destination = actor.position + (vector * tilesize)
@@ -75,6 +63,7 @@ func try_attack_wall(actor: Actor, vector: Vector2) -> bool:
 	var success = tile_map.try_attack_wall(coords)
 	if success:
 		actor.bump_anim(vector)
+		astar_grid.set_point_solid(coords, not tile_map.is_coord_walkable(coords))
 	return success
 	
 func prep_astar_grid() -> void:
@@ -86,5 +75,36 @@ func prep_astar_grid() -> void:
 		for y in astar_grid.region.end.y:
 			if not tile_map.is_coord_walkable(Vector2(x, y)):
 				astar_grid.set_point_solid(Vector2(x, y))
+				
+func hero_moving():
+	if Input.is_action_just_pressed("up"):
+		turn_taken = try_move(hero, Vector2(0, -1))
+	if Input.is_action_just_pressed("down"):
+		turn_taken = try_move(hero, Vector2(0, 1))
+	if Input.is_action_just_pressed("left"):
+		turn_taken = try_move(hero, Vector2(-1, 0))
+	if Input.is_action_just_pressed("right"):
+		turn_taken = try_move(hero, Vector2(1, 0))
+	if Input.is_action_just_pressed("action"):
+		gamestate = ACTION
+		border_gradient_alpha(1.0)
+		
+func hero_action():
+	if Input.is_action_just_pressed("up"):
+		turn_taken = try_attack_wall(hero, Vector2(0, -1))
+		gamestate = MOVING
+		border_gradient_alpha(0.0)
+	if Input.is_action_just_pressed("down"):
+		turn_taken = try_attack_wall(hero, Vector2(0, 1))
+		gamestate = MOVING
+		border_gradient_alpha(0.0)
+	if Input.is_action_just_pressed("left"):
+		turn_taken = try_attack_wall(hero, Vector2(-1, 0))
+		gamestate = MOVING
+		border_gradient_alpha(0.0)
+	if Input.is_action_just_pressed("right"):
+		turn_taken = try_attack_wall(hero, Vector2(1, 0))
+		gamestate = MOVING
+		border_gradient_alpha(0.0)
 	
 		
