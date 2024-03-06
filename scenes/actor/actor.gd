b55d80e94912c42455759f40d0f0cb6c4c3bdf9f
nuzcraft <nuzcraft@gmail.com>
2024-03-05 21:27:22 -0600

    changed player sprite

--- scenes/actor/actor.gd
+++ scenes/actor/actor.gd
@@ -18,6 +18,14 @@ func _process(delta: float) -> void:
 func move(vector: Vector2) -> void:
 	position += vector * tilesize
 	animated_sprite_2d.position -= vector * tilesize
+	if vector.x > 0:
+		animated_sprite_2d.flip_h = false
+	elif vector.x < 0:
+		animated_sprite_2d.flip_h = true 
 	
 func bump_anim(vector: Vector2) -> void:
 	animated_sprite_2d.position += (vector * tilesize) * 0.75
+	if vector.x > 0:
+		animated_sprite_2d.flip_h = false
+	elif vector.x < 0:
+		animated_sprite_2d.flip_h = true 
